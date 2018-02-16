1a9d4fb1fc499aae064dabf614b95cdefeced21f
npartovi <npartovi@gmail.com>
2018-02-15 22:28:31 -0800

    homework update

--- w1d5/DIY_ADT.rb
+++ w1d5/DIY_ADT.rb
@@ -1,28 +1,75 @@
 class Stack
+  def initialize
+    @stack = []
+  end
 
-    def initialize
-      @stack = []
-    end
+  def add(el)
+    @stack << el
+    el
+  end
 
-    def add(el)
-      @stack << el
+  def remove
+    @stack.pop
+  end
 
-    end
-
-    def remove
-      @stack.pop
-    end
-
-    def show
-      @stack.dup
-    end
+  def show
+    @stack.dup
+  end
 end
 
-new_stack = Stack.new
-new_stack.add(3)
-new_stack.add(4)
-new_stack.add(5)
-new_stack.show
+
+
+# EXERCISE 2
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+    el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
+end
 
 
 
+# EXERCISE 3
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    pair_index = @map.index {|pair| pair[0] == key}
+    pair_index ? @map[pair_index][1] = value : @map.push([key, value])
+    [key, value]
+  end
+
+  def lookup(key)
+    @map.each {|pair| return pair[1] if pair[0] == key}
+    nil
+  end
+
+  def remove(key)
+    @map.reject! {|pair| pair[0] == key}
+    nil
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end
+  
+end
