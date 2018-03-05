a5d2ffad20cd7b2154b6813d6d8851db57433ecc
npartovi <npartovi@gmail.com>
2018-03-04 17:10:34 -0800

    W4D1 homework complete

---
+++ W4D1/skeleton/app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save!
+    redirect_to books_url
+  end
+
+  def destroy
+
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_url
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
