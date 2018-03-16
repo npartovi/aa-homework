b08762e8e40e8b1b4f6ee961a3e244222440acab
npartovi <npartovi@gmail.com>
2018-03-15 22:34:10 -0700

    W5D5

---
+++ W5D5/homework.js
@@ -0,0 +1,12 @@
+// window.setTimeout(function(){
+// 	alert('HAMMERTIME');
+// },5000);
+
+
+function hammerTime(time){
+
+	window.setTimeout(function(){
+		alert(`${time} is hammertime!`)
+	})
+
+}
